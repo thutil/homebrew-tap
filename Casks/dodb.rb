54aadaf83170d771113f0b1ba7d07540b1dfc60b
Jirapan <jirapan_yankhan@hotmail.com>
2026-08-24 16:25:39 +0700

    init brew

---
+++ Casks/dodb.rb
@@ -0,0 +1,27 @@
+cask "dodb" do
+  version "0.2.3"
+  sha256 "55122224e5081bf631252493dd413e044ecbfa7e54a9f45bfcbfc8fa9bedc770"
+
+  url "https://github.com/thutil/dodb/releases/download/v#{version}/dodb_#{version}_universal.dmg"
+  name "dodb"
+  desc "Modern Multi-Platform Database Manager for Postgres, MySQL, MariaDB & SQLite"
+  homepage "https://github.com/thutil/dodb"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "dodb.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.thutil.dodb",
+    "~/Library/Caches/com.thutil.dodb",
+    "~/Library/Preferences/com.thutil.dodb.plist",
+    "~/Library/Saved Application State/com.thutil.dodb.savedState",
+    "~/Library/WebKit/com.thutil.dodb",
+  ]
+end
